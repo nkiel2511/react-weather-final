0b0c7adb1ff25bee24f446b8a162809942e482ba
nataliekiel-ux <244553351+nataliekiel-ux@users.noreply.github.com>
2026-02-27 09:34:40 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 export default function App() {
   return 
-    <div className="App">Weather app</div>
+    <div className="App">Weather app
+    <footer>
+      This project was coded by Natalie Kiel and is <a href="https://github.com/nkiel2511/react-weather-final" target="_blank">open-sourced on Github</a>
+      </footer></div>
 ;   
 }
 
