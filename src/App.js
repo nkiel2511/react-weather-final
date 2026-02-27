da5a024556a08d8ba87cffe85b6612b35ecdd80f
nataliekiel-ux <244553351+nataliekiel-ux@users.noreply.github.com>
2026-02-27 09:39:29 +0000

    corrected error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 
 export default function App() {
-  return 
-    <div className="App">Weather app
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
     <footer>
       This project was coded by Natalie Kiel and is <a href="https://github.com/nkiel2511/react-weather-final" target="_blank">open-sourced on Github</a>
-      </footer></div>
-;   
+      </footer>
+      </div>
+  );   
 }
 
 
