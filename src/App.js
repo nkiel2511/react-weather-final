f2b6fb5b14d89672297524c77cfc5bed87c9bfdc
nataliekiel-ux <244553351+nataliekiel-ux@users.noreply.github.com>
2026-02-27 09:41:25 +0000

    fixed error for link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather app</h1>
     <footer>
-      This project was coded by Natalie Kiel and is <a href="https://github.com/nkiel2511/react-weather-final" target="_blank">open-sourced on Github</a>
+      This project was coded by Natalie Kiel and is <a href="https://github.com/nkiel2511/react-weather-final" target="_blank" rel="noreferrer">open-sourced on Github</a>
       </footer>
       </div>
   );   
