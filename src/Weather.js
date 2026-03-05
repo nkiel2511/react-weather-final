110442264dd96a344737a9fe05a7b1cc55c3372b
nataliekiel-ux <244553351+nataliekiel-ux@users.noreply.github.com>
2026-03-05 17:45:08 +0000

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,10 @@ export default function Weather() {
         <form>
             <div className="row">
                 <div className="col-9">
-            <input type="search" placeholder="Enter a city ..." className="form-control"/>
+            <input type="search" placeholder="Enter a city ..." className="form-control" autoFocus="on"/>
             </div>
             <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input type="submit" value="search" className="btn btn-primary w-100" />
              </div>
         </div>
         </form>
@@ -19,12 +19,19 @@ export default function Weather() {
             <li>Wednesday 07:00</li>
             <li>Mostly cloudy </li>
         </ul>
-        <div className="row">
-            <div className="col-6">
+        <div className="row mt-3">
+          <div className="col-6">
+             <div className="clearfix">
                 <img src="https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg"
-                alt="Mostly Cloudy"/>
-                6C
+                alt="Mostly Cloudy"
+                className="float-left"/>
+        
+                <span className="temp float-left">6</span>
+                <span className="unit float-left">°C</span>
+
+              </div>
             </div>
+        </div>
             <div className="col-6">
                 <ul>
                     <li>Percipitation: 15%</li>
@@ -32,6 +39,5 @@ export default function Weather() {
                     <li>Wind: 13 km/h</li>
                 </ul>
             </div>
-    </div>    
     </div>;
 }
