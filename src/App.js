d46aff3e6853f8479d9528dabe613c585c240c62
nataliekiel-ux <244553351+nataliekiel-ux@users.noreply.github.com>
2026-03-05 16:48:32 +0000

    imported bootstrap and added HTML

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather app</h1>
+      <Weather />
     <footer>
       This project was coded by Natalie Kiel and is <a href="https://github.com/nkiel2511/react-weather-final" target="_blank" rel="noreferrer">open-sourced on Github</a>
       </footer>
+      </div>
       </div>
   );   
 }
